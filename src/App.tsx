ca6fbd3a281fc787bf361adc75d8013800f40065
ucho456 <watapqxyz@gmail.com>
2023-03-25 12:32:08 +0900

    set material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Add } from "@mui/icons-material";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <h1>Lite chat</h1>
+      <Add />
     </div>
   );
 }
