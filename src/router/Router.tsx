124a711642a5156544f2093d9ef95594454001cb
ucho456 <watapqxyz@gmail.com>
2023-03-25 17:54:10 +0900

    check for user existence

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import { NavigationGuard } from "./NavigationGuard";
-
+import { useAppSelector } from "../app/hooks";
 import Chat from "../components/chat/Chat";
 import ErrorPage from "../components/error/ErrorPage";
 import Welcome from "../components/welcome/Welcome";
 
-const isAuth = () => {
-  return true;
+const isAuth = (): boolean => {
+  const user = useAppSelector((state) => state.user);
+  return user ? true : false;
 };
 
 export const router = createBrowserRouter([
