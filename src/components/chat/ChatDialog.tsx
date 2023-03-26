9ad685fa90e2ba9a4e12836ecee29ed3a5c6c419
ucho456 <watapqxyz@gmail.com>
2023-03-26 20:50:01 +0900

    fix user name input form

--- src/components/chat/ChatDialog.tsx
+++ src/components/chat/ChatDialog.tsx
@@ -6,10 +6,11 @@ import {
   DialogActions,
   DialogContent,
   DialogContentText,
+  Stack,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { auth, db } from "../../firebase";
-import { useForm } from "react-hook-form";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { updateUser } from "../../features/userSlice";
 import { doc, getDoc } from "firebase/firestore";
@@ -20,10 +21,24 @@ const ChatDialog = () => {
   const navigate = useNavigate();
 
   const [open, setOpen] = useState(true);
+  //Todo: useEffectでroomがnullになったらtrueにしてDialogを出す。
 
-  const { handleSubmit, register, reset, setValue } = useForm<InputUser>({
+  const handleSignOut = () => {
+    auth.signOut();
+    navigate("/welcome");
+  };
+
+  /** Form */
+  const { control, handleSubmit, setValue } = useForm<InputUser>({
     shouldUnregister: false,
+    defaultValues: { name: "", photo: "" },
   });
+
+  const validationRules = {
+    name: {
+      required: "名前を入力してください。",
+    },
+  };
 
   const uid = useAppSelector((state) => state.auth.uid);
   useEffect(() => {
@@ -33,7 +48,6 @@ const ChatDialog = () => {
         const userSnap = await getDoc(userRef);
         if (userSnap.exists()) {
           const user = userSnap.data();
-          dispatch(updateUser(user));
           setValue("name", user.name);
           setValue("photo", user.photo);
         }
@@ -42,48 +56,48 @@ const ChatDialog = () => {
     }
   });
 
-  const handleStart = (inputUser: InputUser) => {
+  const handleStart: SubmitHandler<InputUser> = (inputUser: InputUser) => {
     if (!uid) {
       return;
     }
     //Todo: photoの入力フォームを作成する。
     dispatch(updateUser({ uid, ...inputUser, photo: "" }));
-    reset();
     setOpen(false);
-  };
-
-  //Todo: useEffectでroomがnullになったらtrueにしてDialogを出す。
-
-  const handleSignOut = () => {
-    auth.signOut();
-    navigate("/welcome");
   };
 
   return (
     <div>
       <Dialog open={open}>
-        <form onSubmit={handleSubmit(handleStart)}>
+        <Stack component="form" noValidate onSubmit={handleSubmit(handleStart)}>
           <DialogContent>
             <DialogContentText>
               あなたのプロフィールと相手の条件を入力して開始して下さい。
             </DialogContentText>
-            <TextField
-              fullWidth
-              InputLabelProps={{ shrink: true }}
-              inputProps={{ maxLength: 10 }}
-              label="あなたのニックネーム"
-              margin="dense"
-              {...register("name")}
-              required
-              size="small"
-              type="text"
+            <Controller
+              control={control}
+              name="name"
+              rules={validationRules.name}
+              render={({ field, fieldState }) => (
+                <TextField
+                  error={fieldState.invalid}
+                  {...field}
+                  fullWidth
+                  helperText={fieldState.error?.message}
+                  InputLabelProps={{ shrink: true }}
+                  inputProps={{ maxLength: 10 }}
+                  label="あなたのニックネーム"
+                  margin="normal"
+                  size="small"
+                  type="text"
+                />
+              )}
             />
           </DialogContent>
           <DialogActions>
             <Button onClick={handleSignOut}>サインアウト</Button>
             <Button type="submit">開始する</Button>
           </DialogActions>
-        </form>
+        </Stack>
       </Dialog>
     </div>
   );
