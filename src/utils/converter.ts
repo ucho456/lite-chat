935af960d4aa665fe9c14f1dd7d20f550dea9a80
ucho456 <watapqxyz@gmail.com>
2023-04-07 23:19:43 +0900

    add converterMap

--- src/utils/converter.ts
+++ src/utils/converter.ts
@@ -4,6 +4,64 @@ import type {
   QueryDocumentSnapshot,
 } from "firebase/firestore";
 
+export const messageConverter: FirestoreDataConverter<Message> = {
+  toFirestore(m: Message): DocumentData {
+    return {
+      uid: m.uid,
+      text: m.text,
+      createdAt: m.createdAt,
+    };
+  },
+  fromFirestore(snapshot: QueryDocumentSnapshot): Message {
+    const d = snapshot.data();
+    return {
+      id: snapshot.id,
+      uid: d.uid,
+      text: d.text,
+      createdAt: d.createdAt,
+    };
+  },
+};
+
+export const roomConverter: FirestoreDataConverter<Room> = {
+  toFirestore(r: Room): DocumentData {
+    return {
+      users: r.users,
+      isLeave: r.isLeave,
+      limitAt: r.limitAt,
+    };
+  },
+  fromFirestore(snapshot: QueryDocumentSnapshot): Room {
+    const d = snapshot.data();
+    return {
+      id: snapshot.id,
+      users: d.users,
+      isLeave: d.isLeave,
+      limitAt: d.limitAt,
+    };
+  },
+};
+
+export const signalConverter: FirestoreDataConverter<Signal> = {
+  toFirestore(s: Signal): DocumentData {
+    return {
+      type: s.type,
+      sender: s.sender,
+      sessionDescription: s.sessionDescription,
+      candidate: s.candidate,
+    };
+  },
+  fromFirestore(snapshot: QueryDocumentSnapshot): Signal {
+    const d = snapshot.data();
+    return {
+      type: d.type,
+      sender: d.sender,
+      sessionDescription: d.sessionDescription,
+      candidate: d.candidate,
+    };
+  },
+};
+
 export const userConverter: FirestoreDataConverter<User> = {
   toFirestore(u: User): DocumentData {
     return {
@@ -31,60 +89,16 @@ export const userConverter: FirestoreDataConverter<User> = {
   },
 };
 
-export const roomConverter: FirestoreDataConverter<Room> = {
-  toFirestore(r: Room): DocumentData {
-    return {
-      users: r.users,
-      isLeave: r.isLeave,
-      limitAt: r.limitAt,
-    };
-  },
-  fromFirestore(snapshot: QueryDocumentSnapshot): Room {
-    const d = snapshot.data();
-    return {
-      id: snapshot.id,
-      users: d.users,
-      isLeave: d.isLeave,
-      limitAt: d.limitAt,
-    };
-  },
-};
+type CollectionName = "messages" | "rooms" | "signals" | "users";
+type ConverterType =
+  | FirestoreDataConverter<Message>
+  | FirestoreDataConverter<Room>
+  | FirestoreDataConverter<Signal>
+  | FirestoreDataConverter<User>;
 
-export const messageConverter: FirestoreDataConverter<Message> = {
-  toFirestore(m: Message): DocumentData {
-    return {
-      uid: m.uid,
-      text: m.text,
-      createdAt: m.createdAt,
-    };
-  },
-  fromFirestore(snapshot: QueryDocumentSnapshot): Message {
-    const d = snapshot.data();
-    return {
-      id: snapshot.id,
-      uid: d.uid,
-      text: d.text,
-      createdAt: d.createdAt,
-    };
-  },
-};
-
-export const signalConverter: FirestoreDataConverter<Signal> = {
-  toFirestore(s: Signal): DocumentData {
-    return {
-      type: s.type,
-      sender: s.sender,
-      sessionDescription: s.sessionDescription,
-      candidate: s.candidate,
-    };
-  },
-  fromFirestore(snapshot: QueryDocumentSnapshot): Signal {
-    const d = snapshot.data();
-    return {
-      type: d.type,
-      sender: d.sender,
-      sessionDescription: d.sessionDescription,
-      candidate: d.candidate,
-    };
-  },
-};
+export const converterMap = new Map<CollectionName, ConverterType>([
+  ["messages", messageConverter],
+  ["rooms", roomConverter],
+  ["signals", signalConverter],
+  ["users", userConverter],
+]);
