bd970ec1a4bca839832c81192364d89583b187a8
ucho456 <watapqxyz@gmail.com>
2023-04-09 22:41:44 +0900

    add setUserAsync

--- src/components/rooms/RoomsHeader.tsx
+++ src/components/rooms/RoomsHeader.tsx
@@ -2,14 +2,22 @@ import { Avatar, IconButton } from "@mui/material";
 import "./RoomsHeader.scss";
 import { Logout } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
-import { useAppDispatch } from "../../store/hooks";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { auth } from "../../firebase";
 import { signOut } from "../../store/modules/authSlice";
+import { useEffect } from "react";
+import { setUserAsync } from "../../store/modules/userSlice";
 
 const RoomsHeader = () => {
-  const user = { name: "aa", photo: null };
-
+  const user = useAppSelector((state) => state.user.user);
+  const authUid = useAppSelector((state) => state.auth.uid);
   const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (user || !authUid) return;
+    dispatch(setUserAsync({ authUid }));
+  }, [authUid]);
+
   const navigate = useNavigate();
   const handleSignOut = () => {
     auth.signOut();
@@ -17,17 +25,18 @@ const RoomsHeader = () => {
     navigate("/");
   };
 
+  if (!user) return <></>;
   return (
     <div className="rooms-header">
       <div className="container">
-        <div className="photo-column">
+        <div className="user-column">
           {user.photo ? (
             <Avatar src={user.photo} />
           ) : (
             <Avatar src="/avatar.png" />
           )}
+          <div className="name">{user.name}</div>
         </div>
-        <div className="name-column">{user.name}</div>
         <div className="leave-column">
           <IconButton onClick={() => handleSignOut()}>
             <Logout fontSize="large" />
--- src/store/modules/userSlice.ts
+++ src/store/modules/userSlice.ts
@@ -1,5 +1,12 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  AnyAction,
+  createSlice,
+  PayloadAction,
+  ThunkAction,
+} from "@reduxjs/toolkit";
 import { User } from "../../hooks/useUser";
+import { RootState } from "../store";
+import useUser from "../../hooks/useUser";
 
 type UserState = {
   user: User | null;
@@ -13,11 +20,27 @@ export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    updateUser: (state, action: PayloadAction<UserState["user"]>) => {
+    setUser: (state, action: PayloadAction<UserState["user"]>) => {
       state.user = action.payload;
     },
   },
 });
 
-export const { updateUser } = userSlice.actions;
+const { setUser } = userSlice.actions;
+
+const setUserAsync = (payload: {
+  authUid: string;
+}): ThunkAction<void, RootState, unknown, AnyAction> => {
+  return async (dispatch) => {
+    const { getUserDoc } = useUser();
+    try {
+      const user = await getUserDoc(payload.authUid);
+      dispatch(setUser(user));
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+};
+
+export { setUser, setUserAsync };
 export default userSlice.reducer;
