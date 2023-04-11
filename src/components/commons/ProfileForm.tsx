88e248eef4143d31709adcf81d86dc0254930154
ucho456 <watapqxyz@gmail.com>
2023-04-11 20:35:44 +0900

    add ProfileForm

---
+++ src/components/commons/ProfileForm.tsx
@@ -0,0 +1,94 @@
+import "./ProfileForm.scss";
+import {
+  Avatar,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+} from "@mui/material";
+import {
+  Control,
+  Controller,
+  SubmitHandler,
+  UseFormHandleSubmit,
+} from "react-hook-form";
+import { InputUser } from "../../hooks/useUser";
+
+type Props = {
+  control: Control<InputUser, any>;
+  handleSubmit: UseFormHandleSubmit<InputUser>;
+  handleFunction: SubmitHandler<InputUser>;
+  children: any;
+};
+
+const ProfileForm = ({
+  control,
+  handleSubmit,
+  handleFunction,
+  children,
+}: Props) => {
+  return (
+    <div className="profile-form">
+      <Stack component="form" onSubmit={handleSubmit(handleFunction)}>
+        <div className="container">
+          <div className="me-row">
+            <h3>- あなたのプロフィール -</h3>
+            <div className="container">
+              <div className="photo-column">
+                <Avatar
+                  className="photo"
+                  src="/avatar.png"
+                  sx={{ width: 110, height: 110 }}
+                />
+              </div>
+              <div className="name-column">
+                <Controller
+                  control={control}
+                  name="name"
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      fullWidth
+                      InputLabelProps={{ shrink: true }}
+                      inputProps={{ maxLength: 10 }}
+                      label="ニックネーム"
+                      required
+                      size="small"
+                      type="text"
+                    />
+                  )}
+                />
+              </div>
+              <div className="sex-column">
+                <Controller
+                  control={control}
+                  name="sex"
+                  render={({ field }) => (
+                    <FormControl>
+                      <InputLabel id="me-sex-label">性別</InputLabel>
+                      <Select
+                        {...field}
+                        fullWidth
+                        label="性別"
+                        labelId="me-sex-label"
+                        size="small"
+                      >
+                        <MenuItem value={"man"}>男性</MenuItem>
+                        <MenuItem value={"woman"}>女性</MenuItem>
+                      </Select>
+                    </FormControl>
+                  )}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="button-row">{children}</div>
+        </div>
+      </Stack>
+    </div>
+  );
+};
+
+export default ProfileForm;
--- src/components/top/TopDialog.tsx
+++ src/components/top/TopDialog.tsx
@@ -1,23 +1,12 @@
-import {
-  Avatar,
-  Button,
-  Dialog,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  Stack,
-  TextField,
-} from "@mui/material";
-import { LoadingButton } from "@mui/lab";
+import { Button, Dialog } from "@mui/material";
 import { signInWithPopup } from "firebase/auth";
 import { useState } from "react";
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { auth, googleAuthProvider } from "../../firebase";
-import "./TopDialog.scss";
-import { Send } from "@mui/icons-material";
 import useUser, { InputUser } from "../../hooks/useUser";
+import ProfileForm from "../commons/ProfileForm";
+import { LoadingButton } from "@mui/lab";
 
 const TopDialog = () => {
   const navigate = useNavigate();
@@ -58,73 +47,22 @@ const TopDialog = () => {
         新規登録
       </Button>
       <Dialog className="top-dialog" open={open} onClose={handleClose}>
-        <Stack component="form" onSubmit={handleSubmit(handleSignup)}>
-          <div className="container">
-            <div className="me-row">
-              <h3>- あなたのプロフィール -</h3>
-              <div className="container">
-                <div className="photo-column">
-                  <Avatar
-                    className="photo"
-                    src="/avatar.png"
-                    sx={{ width: 110, height: 110 }}
-                  />
-                </div>
-                <div className="name-column">
-                  <Controller
-                    control={control}
-                    name="name"
-                    render={({ field }) => (
-                      <TextField
-                        {...field}
-                        fullWidth
-                        InputLabelProps={{ shrink: true }}
-                        inputProps={{ maxLength: 10 }}
-                        label="ニックネーム"
-                        required
-                        size="small"
-                        type="text"
-                      />
-                    )}
-                  />
-                </div>
-                <div className="sex-column">
-                  <Controller
-                    control={control}
-                    name="sex"
-                    render={({ field }) => (
-                      <FormControl>
-                        <InputLabel id="me-sex-label">性別</InputLabel>
-                        <Select
-                          {...field}
-                          fullWidth
-                          label="性別"
-                          labelId="me-sex-label"
-                          size="small"
-                        >
-                          <MenuItem value={"man"}>男性</MenuItem>
-                          <MenuItem value={"woman"}>女性</MenuItem>
-                        </Select>
-                      </FormControl>
-                    )}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="button-row">
-              <LoadingButton
-                loading={loading}
-                endIcon={<Send />}
-                loadingPosition="end"
-                size="large"
-                type="submit"
-                variant="contained"
-              >
-                {loading ? "マッチング中..." : "マッチングを開始"}
-              </LoadingButton>
-            </div>
-          </div>
-        </Stack>
+        <ProfileForm
+          control={control}
+          handleFunction={handleSignup}
+          handleSubmit={handleSubmit}
+        >
+          <LoadingButton
+            style={{ backgroundColor: "#4285f4", textTransform: "none" }}
+            loading={loading}
+            startIcon={<img src="/google_icon.png" />}
+            size="large"
+            type="submit"
+            variant="contained"
+          >
+            Googleアカウントで新規登録
+          </LoadingButton>
+        </ProfileForm>
       </Dialog>
     </div>
   );
