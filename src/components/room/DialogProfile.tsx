0384bf530ec3cd28921bb5e11e097455ceef341b
ucho456 <watapqxyz@gmail.com>
2023-04-20 20:20:25 +0900

    add DialogProfile

--- src/components/room/DialogProfile.tsx
+++ src/components/room/DialogProfile.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Avatar, Dialog } from "@mui/material";
+import { ERAS } from "@/utils/constants";
 import "./DialogProfile.scss";
 
 type Props = {
@@ -12,6 +13,10 @@ const DialogProfile = ({ you }: Props) => {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  const sex = you.sex === "man" ? "男性" : "女性";
+  const era = ERAS.find((e) => e.value === you.era);
+
+  if (!era) return <></>;
   return (
     <div className="dialog-profile">
       <div className="user-column" onClick={handleOpen}>
@@ -19,7 +24,21 @@ const DialogProfile = ({ you }: Props) => {
         <div className="name">{you.name}</div>
       </div>
       <Dialog className="dialog-profile" open={open} onClose={handleClose}>
-        aaa
+        <div className="container">
+          <div className="photo-row">
+            <Avatar
+              src={you.photo ?? "/avatar.png"}
+              sx={{ width: 110, height: 110, margin: "0 auto" }}
+            />
+          </div>
+          <div className="profile-row">
+            <p className="name">{you.name}</p>
+            <p>
+              {sex} {era.name}
+            </p>
+            <p>{you.selfIntroduction}</p>
+          </div>
+        </div>
       </Dialog>
     </div>
   );
