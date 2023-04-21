bf90d902c9a478057b283253f22ce7a5f5634c61
ucho456 <watapqxyz@gmail.com>
2023-04-21 18:13:14 +0900

    set vitest

---
+++ src/__tests__/components/commons/AppSnackbar.spec.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import AppSnackbar from "@/components/commons/AppSnackbar";
+
+describe("AppSnackbar", () => {
+  test("renders message when open is true", async () => {
+    const message = "This is a test message";
+    render(<AppSnackbar open={true} message={message} />);
+
+    const messageElement = await screen.findByText(message);
+    expect(messageElement).toBeInTheDocument();
+  });
+
+  test("does not render when open is false", async () => {
+    const message = "This is a test message";
+    render(<AppSnackbar open={false} message={message} />);
+
+    const messageElement = screen.queryByText(message);
+    expect(messageElement).not.toBeInTheDocument();
+  });
+
+  test("renders with the correct severity", async () => {
+    const message = "This is a test message";
+    render(<AppSnackbar open={true} message={message} severity="error" />);
+
+    const alertElement = screen.getByRole("alert");
+    expect(alertElement).toHaveClass("MuiAlert-filledError");
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/__tests__/setup.ts",
+    coverage: {
+      provider: "c8",
+      include: ["src/**/*.{js,ts}"],
+      exclude: ["src/**/__mocks__/**"],
+    },
+  },
 });
