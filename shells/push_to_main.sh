4ced629e5485660191d88ad40c077ecadb32a87c
ucho456 <watapqxyz@gmail.com>
2023-04-29 00:53:42 +0900

    save

--- shells/push_to_main.sh
+++ shells/push_to_main.sh
@@ -1,17 +1,16 @@
-REMOTE_URL="https://github.com/ucho456/lite-chat"
 DEV_BRANCH="dev"
 MAIN_BRANCH="main"
 
 git checkout $DEV_BRANCH
 
-git pull $REMOTE_URL $DEV_BRANCH
+git pull $DEV_BRANCH
 
 git checkout $MAIN_BRANCH
 
-git pull $REMOTE_URL $MAIN_BRANCH
+git pull $MAIN_BRANCH
 
 git merge $DEV_BRANCH
 
-git push $REMOTE_URL $MAIN_BRANCH
+git push $MAIN_BRANCH
 
 git checkout $DEV_BRANCH
