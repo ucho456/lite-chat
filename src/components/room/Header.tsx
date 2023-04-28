2f135e9374a199f81ad32adc1f2adb470d8e428f
ucho456 <95033809+ucho456@users.noreply.github.com>
2023-04-29 00:19:29 +0900

    Componentize the confirmation dialog (#11)

--- src/components/room/Header.tsx
+++ src/components/room/Header.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Block, ChevronLeft, Phone } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
+import { onSnapshot } from "firebase/firestore";
 import { useSnackbar } from "@/contexts/Snackbar";
-import { blockRoom } from "@/utils/firestore";
+import { blockRoom, getCallColRef } from "@/utils/firestore";
+import DialogConfirm from "@/components/commons/DialogConfirm";
 import DialogProfile from "@/components/commons/DialogProfile";
 import "./Header.scss";
 
@@ -11,31 +14,75 @@ type Props = {
   you: RoomUser;
 };
 
+type DialogType = "" | "call" | "answer" | "block";
+
 const Header = ({ me, you }: Props) => {
-  /** Routing */
   const navigate = useNavigate();
   const handleNavigateRooms = (): void => navigate("/rooms");
   const { roomId } = useParams<{ roomId: string }>();
-  const handleNavigatePhone = (): void => {
-    const result = confirm(
-      `${you.name}さんとビデオ電話で通話しますか？（相手がチャットルームにいないと通知されません。）`,
-    );
-    if (result) navigate(`/rooms/${roomId}/phone?caller=true`);
+
+  /** Dialog */
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState("");
+  const [dialogType, setDialogType] = useState<DialogType>("");
+  const handleReject = (): void => {
+    setOpen(false);
+    setMessage("");
+    setDialogType("");
   };
 
+  /** Call phone */
+  const handleCallPhone = (): void => {
+    setDialogType("call");
+    setMessage(
+      `${you.name}さんとビデオ電話で通話しますか？（相手がチャットルームにいないと通知されません。）`,
+    );
+    setOpen(true);
+  };
+  const handlePushToPhone = (): void => {
+    navigate(`/rooms/${roomId}/phone?caller=true`);
+  };
+
+  /** Watch phone request */
+  const [callId, setCallId] = useState<string | null>(null);
+  const handleAgree = () => {
+    setOpen(false);
+    navigate(`/rooms/${roomId}/phone?callId=${callId}`);
+  };
+  useEffect(() => {
+    if (!roomId || !you) return;
+    const callColRef = getCallColRef(roomId);
+    const unsubscribe = onSnapshot(callColRef, async (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        if (doc.exists()) {
+          setCallId(doc.id);
+          setDialogType("answer");
+          setMessage(
+            `${you.name}さんからビデオ電話のリクエストを受けました。通話しますか？`,
+          );
+          setOpen(true);
+        }
+      });
+    });
+    return () => unsubscribe();
+  }, [roomId, you]);
+
+  /** Block user */
   const { openSnackbar } = useSnackbar();
   const handleBlock = (): void => {
+    setDialogType("block");
+    setMessage(`${you.name}さんをブロックしますか？`);
+    setOpen(true);
+  };
+  const handleBlockUser = (): void => {
     if (!roomId) return;
-    const result = confirm(`${you.name}さんをブロックしますか？`);
-    if (result) {
-      try {
-        /** Exit the room before waiting for block results. */
-        blockRoom(roomId, me.uid, you.uid);
-        openSnackbar(`${you.name}さんをブロックしました。`, "success");
-        handleNavigateRooms();
-      } catch {
-        openSnackbar("ブロックに失敗しました。", "error");
-      }
+    try {
+      /** Exit the room before waiting for block results. */
+      blockRoom(roomId, me.uid, you.uid);
+      openSnackbar(`${you.name}さんをブロックしました。`, "success");
+      handleNavigateRooms();
+    } catch {
+      openSnackbar("ブロックに失敗しました。", "error");
     }
   };
 
@@ -51,7 +98,7 @@ const Header = ({ me, you }: Props) => {
           <DialogProfile you={you} />
         </div>
         <div className="phone-column">
-          <IconButton onClick={handleNavigatePhone}>
+          <IconButton onClick={handleCallPhone}>
             <Phone fontSize="large" />
           </IconButton>
         </div>
@@ -61,6 +108,18 @@ const Header = ({ me, you }: Props) => {
           </IconButton>
         </div>
       </div>
+      <DialogConfirm
+        open={open}
+        onClickAgree={
+          dialogType === "call"
+            ? handlePushToPhone
+            : dialogType === "answer"
+            ? handleAgree
+            : handleBlockUser
+        }
+        onClickReject={handleReject}
+        message={message}
+      />
     </div>
   );
 };
