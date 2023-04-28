0936ecbf8b654939848c4a32d78611ca9635a7ee
ucho456 <watapqxyz@gmail.com>
2023-04-29 00:58:12 +0900

    save

--- shells/push_to_main.sh
+++ shells/push_to_main.sh
@@ -1,16 +1,5 @@
-DEV_BRANCH="dev"
-MAIN_BRANCH="main"
+git checkout main
 
-git checkout $DEV_BRANCH
+git merge origin dev
 
-git pull $DEV_BRANCH
-
-git checkout $MAIN_BRANCH
-
-git pull $MAIN_BRANCH
-
-git merge $DEV_BRANCH
-
-git push $MAIN_BRANCH
-
-git checkout $DEV_BRANCH
+git push origin main
