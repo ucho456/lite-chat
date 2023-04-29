c3d21098fa854916f885d25d3469800bd7b222ab
ucho456 <95033809+ucho456@users.noreply.github.com>
2023-04-29 22:46:24 +0900

    Unsubscribe from the subscriptions for call, offerCandidate, and answerCandidate (#21)

--- src/components/phone/Header.tsx
+++ src/components/phone/Header.tsx
@@ -2,7 +2,6 @@ import { Component, ReactNode } from "react";
 import { Link } from "react-router-dom";
 import { ChevronLeft } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
-import { Unsubscribe } from "firebase/firestore";
 import { deletePhoneDocs } from "@/utils/firestore";
 import DialogProfile from "@/components/commons/DialogProfile";
 import "./Header.scss";
@@ -13,9 +12,6 @@ type Props = {
   callId: string | null;
   roomId: string;
   you: RoomUser;
-  callUnsubscribe: Unsubscribe | null;
-  offerUnsubscribe: Unsubscribe | null;
-  answerUnsubscribe: Unsubscribe | null;
 };
 
 class Header extends Component<Props> {
@@ -25,10 +21,6 @@ class Header extends Component<Props> {
     if (this.props.callId) {
       deletePhoneDocs(this.props.roomId, this.props.callId);
     }
-    console.log("呼ばれた？");
-    if (this.props.callUnsubscribe) this.props.callUnsubscribe();
-    if (this.props.offerUnsubscribe) this.props.offerUnsubscribe();
-    if (this.props.answerUnsubscribe) this.props.answerUnsubscribe();
   }
   render(): ReactNode {
     return (
