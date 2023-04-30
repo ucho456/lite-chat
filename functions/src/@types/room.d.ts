7b3ee7fcff6c7bef71b4399009f1e73b5835bb09
ucho456 <95033809+ucho456@users.noreply.github.com>
2023-04-30 19:29:47 +0900

    Revert "Organizing cloud functions (#28)"
    This reverts commit 33b59dc61ae56a355a69b793fff66734569b3fe7.

---
+++ functions/src/@types/room.d.ts
@@ -0,0 +1,18 @@
+type RoomUser = {
+  uid: string;
+  name: string;
+  photo: string | null;
+};
+
+type Room = {
+  id: string;
+  users: {
+    A: RoomUser;
+    B: RoomUser;
+  };
+  isLeave: boolean;
+  limitAt: import("firebase-admin/firestore").Timestamp;
+};
+
+// このあとやること
+// RoomとMessageを直接指定してまずは取得しちゃう。
