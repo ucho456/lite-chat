5bd81f6f7844f7be18cb5c8232211426be0fd218
ucho456 <watapqxyz@gmail.com>
2023-04-30 12:48:33 +0900

    Display when the user has been blocked by the other party

--- src/components/error/index.tsx
+++ src/components/error/index.tsx
@@ -1,6 +1,8 @@
+import "./index.scss";
+
 const Error = () => {
   return (
-    <div>
+    <div className="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
     </div>
