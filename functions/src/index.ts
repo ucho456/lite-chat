b5c83acbee0db3b959dd1e1830ba928fe42a1b77
ucho456 <95033809+ucho456@users.noreply.github.com>
2023-04-30 19:30:52 +0900

    Revert "Organizing cloud functions" (#35)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,8 @@
 import * as admin from "firebase-admin";
 admin.initializeApp();
 
-// import updatePaidMember from "./updatePaidMember";
+// import matching from "./matching";
 
 module.exports = {
-  // updatePaidMember,
+  // matching,
 };
