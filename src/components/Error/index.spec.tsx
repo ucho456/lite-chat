9a9b0f00fab0a4181fdad87dc86ca7a063ef16df
Yuto Araki <95033809+ucho456@users.noreply.github.com>
2023-07-07 15:19:05 +0900

    Add pull request template (#122)

--- src/components/Error/index.spec.tsx
+++ src/components/Error/index.spec.tsx
@@ -1,10 +1,12 @@
-import { render } from '@testing-library/react';
-import Error from '@/components/Error';
+import { render } from "@testing-library/react";
+import Error from "@/components/Error";
 
-describe('Error component', () => {
-  it('renders the correct content', () => {
+describe("Error component", () => {
+  it("renders the correct content", () => {
     const { getByText } = render(<Error />);
-    expect(getByText('Oops!')).toBeInTheDocument();
-    expect(getByText('Sorry, an unexpected error has occurred.')).toBeInTheDocument();
+    expect(getByText("Oops!")).toBeInTheDocument();
+    expect(
+      getByText("Sorry, an unexpected error has occurred."),
+    ).toBeInTheDocument();
   });
 });
